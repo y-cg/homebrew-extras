92787a2e344344dc65618e39df25a406f7d53ce8
YCG <devin.cg.yeung@gmail.com>
2024-03-19 17:14:34 +0800

    feat: clash-verge-rev

---
+++ Casks/clash-verge-rev.rb
@@ -0,0 +1,40 @@
+cask "clash-verge-rev" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "1.5.9"
+  sha256 arm:   "c8255335ea4a31fd687bcb4d132442263ec3a6c12eba6ccf31bd8a050aeefd07",
+         intel: "c539c918766c54c0214fcdc32fddb1be8df3f74f03a35c2b1db7fbeda23ba3a9"
+
+
+  url "https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
+
+  name "Clash Verge Rev"
+
+  desc "Continuation of Clash Verge - A Clash Meta GUI based on Tauri (Windows, MacOS, Linux)"
+
+  homepage "https://github.com/clash-verge-rev/clash-verge-rev"
+
+  livecheck do
+    url url.to_s
+    strategy :github_latest
+  end
+
+  auto_updates true
+  conflicts_with cask: "clash-verge"
+
+  app "Clash Verge.app"
+
+  preflight do
+    system_command "xattr",
+                   args: ["-cr", "#{staged_path}/Clash Verge.app"]
+  end
+
+  zap trash: [
+    "~/.config/clash-verge",
+    "~/Library/Application Support/io.github.clash-verge-rev.clash-verge-rev",
+    "~/Library/Caches/io.github.clash-verge-rev.clash-verge-rev",
+    "~/Library/Preferences/io.github.clash-verge-rev.clash-verge-rev.savedState.plist",
+    "~/Library/Saved Application State/io.github.clash-verge-rev.clash-verge-rev.savedState",
+    "~/Library/WebKit/io.github.clash-verge-rev.clash-verge-rev",
+  ]
+end
