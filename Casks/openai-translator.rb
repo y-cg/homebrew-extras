7934efb7772aea72dc4ab099277a61f83688d4bf
y-cg <devin.cg.yeung@gmail.com>
2024-03-24 22:25:41 +0800

    feat: bump openai-translator version

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,8 +1,9 @@
 cask "openai-translator" do
-  version "0.0.19"
-  sha256 "bfc6130788bb4d03a8ab6a0b24419437e7378eb63c5840341d7322de77709c52"
+  version "0.3.34"
+  sha256 x64: "165308eeda3b586b3f7be08effe14f73507e9a775087fd3f7622c2cb6d3761d4"
+         aarch64: "8b8e07f03524135968f8114193e18e9bb4310d3b94ed1158333cc16ab9acfb51"
 
-  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_universal.dmg"
+  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
 
   name "OpenAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
