41c8539fbd8f8b6444eee8773caa3e10784c8a20
y-cg <devin.cg.yeung@gmail.com>
2024-03-24 22:31:12 +0800

    fix the arch again

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,4 +1,6 @@
 cask "openai-translator" do
+  arch arm: "aarch64", intel: "x64"
+
   version "0.3.34"
   sha256 intel: "165308eeda3b586b3f7be08effe14f73507e9a775087fd3f7622c2cb6d3761d4",
          arm: "8b8e07f03524135968f8114193e18e9bb4310d3b94ed1158333cc16ab9acfb51"
