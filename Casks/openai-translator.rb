b49811d384fe237a9d8a85d300d78e34cfcb6b7f
y-cg <devin.cg.yeung@gmail.com>
2024-03-24 22:27:08 +0800

    fix the missing comma

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,6 +1,6 @@
 cask "openai-translator" do
   version "0.3.34"
-  sha256 x64: "165308eeda3b586b3f7be08effe14f73507e9a775087fd3f7622c2cb6d3761d4"
+  sha256 x64: "165308eeda3b586b3f7be08effe14f73507e9a775087fd3f7622c2cb6d3761d4",
          aarch64: "8b8e07f03524135968f8114193e18e9bb4310d3b94ed1158333cc16ab9acfb51"
 
   url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
